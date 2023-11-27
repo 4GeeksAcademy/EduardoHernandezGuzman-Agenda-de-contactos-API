8ef6e1335c087dd011f1a778438d7419c43cf0c6
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2023-11-27 12:04:22 +0000

    Actualización

--- src/js/views/agenda(copia).js
+++ src/js/views/agenda(copia).js
@@ -7,7 +7,7 @@ import { Context } from "../store/appContext";
 
 import "../../styles/demo.css";
 
-export const Demo = () => {
+export const Agenda = () => {
 	const { store, actions } = useContext(Context);
 
 	
@@ -22,6 +22,13 @@ export const Demo = () => {
 							<li
 								key={index}
 								className="list-group-item d-flex ">
+
+								<Link to="/editarcontacto">
+									<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+										<path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+									</svg>
+								</Link>
+
 
 								<div><svg xmlns="http://www.w3.org/2000/svg" width="86" height="86" fill="currentColor" className="mx-5 mt-2 bi bi-person-lines-fill" viewBox="0 0 16 16">
 									<path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5m.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1z" />
@@ -47,6 +54,7 @@ export const Demo = () => {
 										<path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z" />
 									</svg><span className="mx-2">{item.email}</span>
 								</div>
+
 								<button style={{ marginLeft: "auto" }} className="btn btn-danger" onClick={() => { actions.borrarContacto(item.id) }}>Eliminar contacto</button>
 
 							</li>
@@ -56,14 +64,14 @@ export const Demo = () => {
 				})}
 			</ul>
 			<button
-				style={{ marginTop: "3rem" , width:"14rem" , height:"5rem", marginBottom:"3rem" }}
+				style={{ marginTop: "3rem", width: "14rem", height: "5rem", marginBottom: "3rem" }}
 				className="btn btn-danger"
 				onClick={() => actions.borrarTodosLosContactos()}>
-				<span><svg style={{marginBottom:"0.5rem"}} xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-exclamation-triangle" viewBox="0 0 16 16">
+				<span><svg style={{ marginBottom: "0.5rem" }} xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-exclamation-triangle" viewBox="0 0 16 16">
 					<path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z" />
 					<path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
 				</svg>
-				</span> 
+				</span>
 				<h6>Borrar todos los contactos</h6>
 			</button>
 
@@ -91,7 +99,7 @@ export const Demo = () => {
 					);
 				})}
 			</ul> */}
-			
+
 		</div>
 	);
 };
