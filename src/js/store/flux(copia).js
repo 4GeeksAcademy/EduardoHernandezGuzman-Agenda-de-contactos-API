35efd3d012b118149de1d88a2241f827de4a408f
Edo <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2023-11-27 10:22:17 +0000

    Actualización

---
+++ src/js/store/flux(copia).js
@@ -0,0 +1,127 @@
+const getState = ({ getStore, getActions, setStore }) => {
+	return {
+		store: {
+			// demo: [
+			// 	{
+			// 		title: "FIRST",
+			// 		background: "white",
+			// 		initial: "white"
+			// 	},
+			// 	{
+			// 		title: "SECOND",
+			// 		background: "white",
+			// 		initial: "white"
+			// 	}
+				
+			// ],
+			contacts: [
+				{
+					full_name: "",
+					address: "",
+					phone: "",
+					email: ""
+				},
+				{
+					full_name: "",
+					address: "",
+					phone: "",
+					email: ""
+				},
+
+			]
+		},
+		actions: {
+			// Use getActions to call a function within a fuction
+			// exampleFunction: () => {
+			// 	getActions().changeColor(0, "green");
+			// },
+			loadSomeData: () => {
+
+				
+				// setStore({ contacts: [
+				// 	{
+				// 		full_name: "CAmbiado",
+				// 		address: "CAmbiado",
+				// 		phone: "CAmbiado",
+				// 		email: "rwCAmbiadoe"
+				// 	},
+				// 	{
+				// 		full_name: "nuevo cambio",
+				// 		address: "nuevo cambio",
+				// 		phone: "nuevo cambio",
+				// 		email: "nuevo cambio"
+				// 	},
+	
+				// ] })
+
+
+				//Traer los contactos de la agenda
+				fetch("https://playground.4geeks.com/apis/fake/contact/agenda/miagenda")
+				.then((response)=> response.json())
+				.then((data)=>setStore({ contacts: data }))
+
+				/**
+					fetch().then().then(data => setStore({ "foo": data.bar }))
+				*/
+
+				// var requestOptions = {
+				// 	method: 'GET',
+				// 	redirect: 'follow'
+				//   };
+				  
+				//   fetch("https://playground.4geeks.com/apis/fake/contact/agenda/miagenda", requestOptions)
+				// 	.then(response => response.json())
+				// 	.then(data => setStore({ contacts: data }))
+			},
+
+			//Borrar un contacto de la agenda
+			borrarContacto: (indexABorrar) => {
+
+				var requestOptions = {
+					method: 'DELETE',
+					redirect: 'follow'
+				};
+
+				fetch("https://playground.4geeks.com/apis/fake/contact/" + indexABorrar, requestOptions)
+					.then(response => response.json())
+					.then(data => setStore({ contacts: data }))
+
+					setTimeout(() => { location.reload(); }, 1000); //Tengo que buscar solución para este mal arreglo
+					
+
+
+			},
+
+			//Borrar TODOS los contactos de la agenda
+			borrarTodosLosContactos: () => {
+
+				var requestOptions = {
+					method: 'DELETE',
+					redirect: 'follow'
+				};
+
+				fetch("https://playground.4geeks.com/apis/fake/contact/agenda/miagenda", requestOptions)
+					.then(response => response.json())
+					.then(data => setStore({ contacts: data }))
+				setTimeout(() => { location.reload(); }, 1000); //Tengo que buscar solución para este mal arreglo
+			},
+
+			// changeColor: (index, color) => {
+			// 	//get the store
+			// 	const store = getStore();
+
+			// 	//we have to loop the entire demo array to look for the respective index
+			// 	//and change its color
+			// 	const demo = store.demo.map((elm, i) => {
+			// 		if (i === index) elm.background = color;
+			// 		return elm;
+			// 	});
+
+			// 	//reset the global store
+			// 	setStore({ demo: demo });
+			// }
+		}
+	};
+};
+
+export default getState;
--- src/js/views/home(copia).js
+++ src/js/views/home(copia).js
@@ -8,6 +8,7 @@ export const Home = () => {
     const [phone, setPhone] = useState("");
     const [confirmationMsg, setConfirmationMsg] = useState("");
 
+
     // Creación de un nuevo contacto
     function nuevoContacto() {
         var myHeaders = new Headers();
@@ -44,66 +45,82 @@ export const Home = () => {
             })
             .catch(error => {
                 console.error('Error saving contact:', error);
-                setConfirmationMsg("Error saving contact");
+                setConfirmationMsg("Error al guardar el contacto");
             });
     }
 
+
     return (
-        <>
-            <div className="mb-3">
-                <label htmlFor="fullName" className="form-label">Nombre completo</label>
-                <input
-                    onChange={(e) => setFullName(e.target.value)}
-                    value={fullName}
-                    type="text"
-                    className="form-control"
-                    id="fullName"
-                    placeholder="Edward Alexander Crowley"
-                />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="email" className="form-label">Email</label>
-                <input
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                    type="email"
-                    className="form-control"
-                    id="email"
-                    placeholder="aleister@crowley.uk"
-                />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="address" className="form-label">Dirección</label>
-                <input
-                    onChange={(e) => setAddress(e.target.value)}
-                    value={address}
-                    type="text"
-                    className="form-control"
-                    id="address"
-                    placeholder="Inverness IV2 6XT, Reino Unido"
-                />
-            </div>
-            <div className="mb-3">
-                <label htmlFor="phone" className="form-label">Número de teléfono</label>
-                <input
-                    onChange={(e) => setPhone(e.target.value)}
-                    value={phone}
-                    type="text"
-                    className="form-control"
-                    id="phone"
-                    placeholder="666-66666666-66"
-                />
-            </div>
-            {confirmationMsg && <p>{confirmationMsg}</p>}
-            <button
-                onClick={() => {
-                    nuevoContacto();
-                }}
-                type="button"
-                className="btn btn-primary"
-            >
-                Guardar contacto
-            </button>
-        </>
+		<>
+			<div className="m-5 p-3 bg-body-tertiary rounded-3 col-8">
+
+				<h1 className="text-body-emphasis mb-5 display-6">Formulario para añadir contactos</h1>
+				<div className="mb-4 col-6">
+					<label htmlFor="fullName" className="form-label h6">Nombre completo</label>
+					<input
+						onChange={(e) => setFullName(e.target.value)}
+						value={fullName}
+						type="text"
+						className="form-control"
+						id="fullName"
+						placeholder="Edward Alexander Crowley" />
+				</div>
+
+				<div className="mb-4 col-6">
+					<label htmlFor="email" className="form-label h6">Email</label>
+					<input
+						onChange={(e) => setEmail(e.target.value)}
+						value={email}
+						type="email"
+						className="form-control"
+						id="email"
+						placeholder="aleister@crowley.uk" />
+				</div>
+
+				<div className="mb-4 col-6">
+					<label htmlFor="address" className="form-label h6">Dirección</label>
+					<input
+						onChange={(e) => setAddress(e.target.value)}
+						value={address}
+						type="text"
+						className="form-control"
+						id="address"
+						placeholder="Inverness IV2 6XT, Reino Unido"/>
+				</div>
+
+				<div className="mb-4 col-6">
+					<label htmlFor="phone" className="form-label h6">Número de teléfono</label>
+					<input
+						onChange={(e) => setPhone(e.target.value)}
+						value={phone}
+						type="text"
+						className="form-control"
+						id="phone"
+						placeholder="666-66666666-66" />
+				</div>
+
+				<div className="d-inline-flex gap-2">
+					<button
+						style={{marginTop:"3rem"}}
+						onClick={() => { nuevoContacto(); }}
+						className="btn btn-outline-secondary btn-lg px-4 rounded-3"
+						type="button">
+						Guardar contacto
+					</button>
+
+				</div>
+				<div className="container text-center">
+					<div className="row align-items-start">
+						<div className="col-3">
+							{confirmationMsg && <div style={{ height: "5rem", marginTop:"3rem" }} className="alert alert-success d-flex align-items-center" role="alert">
+								<div>
+									{confirmationMsg}
+								</div>
+							</div>}
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
     );
 };
